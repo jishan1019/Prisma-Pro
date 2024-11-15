88d5c7348e8db6b8e6e061b61917a45115e52a2f
jishan1019 <imransilamjishan80@gmail.com>
2024-11-15 15:06:43 +0600

    practice relational filter

---
+++ src/relation-query.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  //fluent api mean post get by user id
+  //   const result = await prisma.user
+  //     .findUnique({
+  //       where: {
+  //         id: 1,
+  //       },
+  //     })
+  //     .post();
+
+  //populate
+  //   const result = await prisma.user.findUnique({
+  //     where: {
+  //       id: 1,
+  //     },
+  //     include: {
+  //       post: true,
+  //     },
+  //   });
+
+  const relationalFields = await prisma.user.findMany({
+    include: {
+      post: {
+        where: {
+          published: true,
+        },
+      },
+    },
+  });
+
+  console.dir(relationalFields, { depth: Infinity });
+};
+
+main();
