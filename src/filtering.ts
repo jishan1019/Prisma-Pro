740a9078d44ad09f40ba366c5bc1f19dc1ccb304
jishan1019 <imransilamjishan80@gmail.com>
2024-11-17 14:37:08 +0600

    practice filter

--- src/filtering.ts
+++ src/filtering.ts
@@ -16,22 +16,59 @@ const main = async () => {
   //     },
   //   });
 
-  const orFiltering = await prisma.post.findMany({
+  //   const orFiltering = await prisma.post.findMany({
+  //     where: {
+  //       OR: [
+  //         {
+  //           title: {
+  //             contains: "title",
+  //           },
+  //         },
+  //         {
+  //           published: true,
+  //         },
+  //       ],
+  //     },
+  //   });
+
+  //   const notFiltering = await prisma.post.findMany({
+  //     where: {
+  //       NOT: [
+  //         {
+  //           title: {
+  //             contains: "title",
+  //           },
+  //         },
+  //         {
+  //           published: true,
+  //         },
+  //       ],
+  //     },
+  //   });
+
+  //   const startWithFiltering = await prisma.user.findMany({
+  //     where: {
+  //       email: {
+  //         startsWith: "user",
+  //       },
+  //     },
+  //   });
+
+  const endWithFiltering = await prisma.user.findMany({
     where: {
-      OR: [
-        {
-          title: {
-            contains: "title",
-          },
-        },
-        {
-          published: true,
-        },
-      ],
+      email: {
+        endsWith: "com",
+      },
     },
   });
 
-  console.log(orFiltering);
+  const equalFiltering = await prisma.user.findMany({
+    where: {
+      email: "user1@gmail.com",
+    },
+  });
+
+  console.log(equalFiltering);
 };
 
 main();
